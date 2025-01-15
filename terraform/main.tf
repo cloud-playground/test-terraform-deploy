a5001dcf1cec2eb9980bf99b2fdb58040f3bc8ab
cloud-playground <cloud_playground@outlook.com>
2025-01-15 14:29:08 +1100

    add apply

--- terraform/main.tf
+++ terraform/main.tf
@@ -16,6 +16,6 @@ resource "azurerm_linux_web_app" "testdeploy-app" {
   resource_group_name = data.azurerm_resource_group.test_rg.name
   name = "test-app"
   location = "australiaeast"
-  service_plan_id = azurerm_app_service_plan.test-app
+  service_plan_id = azurerm_app_service_plan.test-app.id
   site_config {}
 }
